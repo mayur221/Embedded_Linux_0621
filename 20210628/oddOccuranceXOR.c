54729719ebea3dcd71a8b1445f58c3631a72ee07
mayur221 <85892363+mayur221@users.noreply.github.com>
2021-06-30 20:02:55 +0530

    Update oddOccuranceXOR.c

--- 20210628/oddOccuranceXOR.c
+++ 20210628/oddOccuranceXOR.c
@@ -11,7 +11,6 @@ int getOddOccurrence(int ar[], int ar_size)
  
 int main()
 {
-    //int ar[] = {2, 3, 5, 4, 5, 2, 4, 3, 5, 2, 4, 4, 2};
     int ar[] = {20,20,40,40,13,13,11,88,88,98,98,11,11};
     int n = sizeof(ar) / sizeof(ar[0]);
      
